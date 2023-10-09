4e8a7843a801a3e93082fed28a7d54367b4c4ff0
MaalejAyman <78954935+MaalejAyman@users.noreply.github.com>
2023-10-09 23:20:19 +0100

    Initial commit

---
+++ user/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class User(models.Model):
+    email = models.EmailField(primary_key=True)
+    password=models.CharField(max_length=255)
+    pin = models.CharField(unique=True,max_length=4)
+    token=models.CharField(unique=True,max_length=255)
