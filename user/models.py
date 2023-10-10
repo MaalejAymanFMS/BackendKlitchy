439143c5e190c247c7481a7169b3dc27ec2153e3
MaalejAyman <78954935+MaalejAyman@users.noreply.github.com>
2023-10-11 00:50:26 +0100

    added role

--- user/models.py
+++ user/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class User(models.Model):
     email = models.EmailField(primary_key=True)
     password=models.CharField(max_length=255)
-    role=models.CharField(max_length=255)
+    role=models.CharField(max_length=255,default="null")
     pin = models.CharField(unique=True,max_length=4)
     token=models.CharField(unique=True,max_length=255)
